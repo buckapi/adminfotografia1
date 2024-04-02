5f83d39f696d4d6818ebfea3ec1f2367d76ac229
root <root@localhost.localdomain>
2024-04-02 18:29:12 -0500

    carga de imagenes en edit

--- src/app/components/albums/edittest/edittest.component.ts
+++ src/app/components/albums/edittest/edittest.component.ts
@@ -1,15 +1,97 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit ,AfterViewInit,ViewChild} from '@angular/core';
+import { DemoFilePickerAdapter } from  '@app/file-picker.adapter';
+import { HttpClient } from '@angular/common/http';
+import { Butler } from '@app/services/butler.service';
+import { DataApiService } from '@app/services/data-api.service';
+import { UploaderCaptions } from 'ngx-awesome-uploader';
+import { Yeoman } from '@services/yeoman.service';
+import Swal from 'sweetalert2'
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-edittest',
   templateUrl: './edittest.component.html',
   styleUrls: ['./edittest.component.css']
 })
-export class EdittestComponent implements OnInit {
+export class EdittestComponent implements AfterViewInit {
+  @ViewChild('deleteSwal')
+  category='Seleccione una';
+  categorySeted:boolean=false;
+  imagesToDelete: boolean[] = [];
+  public captions: UploaderCaptions = {
+    dropzone: {
+      title: 'Imágenes del producto',
+      or: '.',
+      browse: 'Cargar',
+    },
+    cropper: {
+      crop: 'Cortar',
+      cancel: 'Cancelar',
+    },
+    previewCard: {
+      remove: 'Borrar',
+      uploadError: 'error',
+    },
+  };
+  data = {
+    images: [] as string[], // o cualquier otro tipo de dato adecuado, como any[]
+    name: '',
+    ref: '',
+    idCategory: '',
+  };adapter = new  DemoFilePickerAdapter(this.http,this._butler);
+  constructor(
+    public router:Router,
+    public http:HttpClient,
+    public _butler:Butler,
+    public dataApiService:DataApiService,
+    public yeoman:Yeoman) 
+    {
+      this.data=this.yeoman.preview;
 
-  constructor() { }
-
-  ngOnInit(): void {
+     }
+     cancelarUpdate(){
+      this.router.navigate(['/albAll']);
+    }
+    onSubmit() {
+      // Obtener las imágenes actuales del álbum
+      const currentImages = this.yeoman.preview.images;
+  
+      // Actualizar this.data.images con las imágenes actuales
+      this.data.images = currentImages;
+  
+      // Verificar si hay nuevas imágenes seleccionadas para cargar
+      if (this._butler.uploaderImages.length > 0) {
+          // Agregar las nuevas imágenes al array de imágenes
+          this.data.images.push(...this._butler.uploaderImages);
+  
+          // Limpiar el cargador de imágenes
+          this._butler.uploaderImages = [];
+      }
+  
+      // Realizar la actualización del álbum con las imágenes actuales y nuevas
+      this.dataApiService.albumsUpdate(this.data, this.yeoman.preview.id).subscribe(response => {
+          console.log(response);
+          this.router.navigate(['albAll']);
+          this.data = {
+              images: [] as string[], // o cualquier otro tipo de dato adecuado, como any[]
+              name: '',
+              ref: '',
+              idCategory: ''
+          };
+          Swal.fire({
+              position: 'center',
+              icon: 'success',
+              title: 'Album Actualizado',
+              showConfirmButton: false,
+              timer: 1500
+          });
+      });
+  }
+  
+  
+  
+  
+  ngAfterViewInit(): void {
   }
 
 }
